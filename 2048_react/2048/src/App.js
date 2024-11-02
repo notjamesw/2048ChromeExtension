3461d2f2370fe1db75f51aa563e30dc606341bfa
jameswen <leranjamesw@gmail.com>
2024-11-01 23:39:36 -0700

    carried over static html from previous version

--- 2048_react/2048/src/App.js
+++ 2048_react/2048/src/App.js
@@ -5,7 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>2048</h1>
+        <div>
+            <button class = "button">New Game</button>
+            <div class = "scores">
+                Score: <span id = "score"> 0 </span> Best: <span id = "highscore"> 0 </span>
+            </div>
+        </div>
+        <div class = "container">
+            <div>board</div>
+            <div class="popup">
+                <h2>Game Over</h2>
+                <button class = "button">New Game</button>
+            </div>
+            <div></div>
+        </div>
       </header>
     </div>
   );
